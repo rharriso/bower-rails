6197ca146345751c5a78404e21610c2cbfd6d30d
Sergey Kishenin <sergey.kishenin@gmail.com>
2014-01-04 16:20:21 +0600

    put coveralls to the very top of the file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
+require 'coveralls'
+Coveralls.wear!
+
 $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'rspec'
 require 'bower-rails'
-require 'coveralls'
-Coveralls.wear!
