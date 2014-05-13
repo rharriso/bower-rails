f6e80f5bd972ebb17f21836aa088d3bf30786ad7
DadoCe <dado.ce@gmail.com>
2014-05-13 10:44:41 -0300

    Fix clean_before_precompile configurable template

--- lib/generators/bower_rails/initialize/templates/bower_rails.rb
+++ lib/generators/bower_rails/initialize/templates/bower_rails.rb
@@ -4,7 +4,7 @@ BowerRails.configure do |bower_rails|
 
   # Invoke `rake bower:resolve` automatically before the `rake assets:precompile` task.
   # bower_rails.resolve_before_precompile = true
-  
+
   # Invoke `rake bower:clean` automatically before the `rake assets:precompile` task.
-  # bower_rails.resolve_before_precompile = true  
+  # bower_rails.clean_before_precompile = true  
 end
