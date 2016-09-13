98451ad0a7996d3d4b70f1a2fe5cbc6696ee6f3d
Anton Dieterle <antondie@gmail.com>
2016-09-13 15:29:34 +0300

    Put url parameters outside of erb on paths resolve

--- spec/bower-rails/performer_spec.rb
+++ spec/bower-rails/performer_spec.rb
@@ -3,25 +3,40 @@ require 'bower-rails/performer'
 require 'pry'
 
 describe BowerRails::Performer do
-
   let(:performer) { BowerRails::Performer.new }
   let(:main_files) { {} }
   let(:exempt_list) { nil }
+  let(:root) { File.expand_path('../../..', __FILE__) }
+  let(:tmp_dir) { File.join root, "tmp" }
+  let(:files_dir) { File.join root, "spec", "files" }
+  let(:create_bower_file) { true }
 
-  context "remove_extra_files" do
-    let(:root) { File.expand_path('../../..', __FILE__) }
-    let(:create_bower_file) { true }
+  before do
+    # `rm -rf ./tmp` remove temporary directory
+    FileUtils.rm_rf("#{root}/tmp")
 
+    # setup temporary directory
+    FileUtils.mkdir("#{root}/tmp")
+    FileUtils.cp("#{root}/spec/files/Bowerfile", "#{root}/tmp/Bowerfile") if create_bower_file
+    FileUtils.cp("#{root}/spec/files/bower.json", "#{root}/tmp/bower.json")
+
+    # points `root_path` to temporary directory
+    allow(performer).to receive(:root_path) { "#{root}/tmp" }
+
+    # trick BowerRails that system has bower installed
+    allow(performer).to receive(:find_command) { "bower" }
+
+    # sets main_files in DSL
+    allow_any_instance_of(BowerRails::Dsl).to receive(:main_files){ main_files }
+
+    Dir.chdir("#{root}/tmp")
+
+    # Stub exclude from clean setting
+    BowerRails.exclude_from_clean = exempt_list
+  end
+
+  describe "remove_extra_files" do
     before do
-      # `rm -rf ./tmp` remove temporary directory
-      FileUtils.rm_rf("#{root}/tmp")
-
-      # setup temporary directory
-      FileUtils.mkdir("#{root}/tmp")
-      FileUtils.cp("#{root}/spec/files/Bowerfile", "#{root}/tmp/Bowerfile") if create_bower_file
-      FileUtils.cp("#{root}/spec/files/bower.json", "#{root}/tmp/bower.json")
-
-
       FileUtils.mkdir_p("#{root}/tmp/vendor/assets/bower_components")
 
       # creates bower library
@@ -37,20 +52,6 @@ describe BowerRails::Performer do
       File.open("#{root}/tmp/vendor/assets/bower_components/moment/bower.json", "w") do |f|
         f.write(%q({"name":"moment","main":["./moment.js", "./fonts/*"]}))
       end
-
-      # points `root_path` to temporary directory
-      allow(performer).to receive(:root_path) { "#{root}/tmp" }
-
-      # trick BowerRails that system has bower installed
-      allow(performer).to receive(:find_command) { "bower" }
-
-      # sets main_files in DSL
-      allow_any_instance_of(BowerRails::Dsl).to receive(:main_files){ main_files }
-
-      Dir.chdir("#{root}/tmp")
-
-      # Stub exclude from clean setting
-      BowerRails.exclude_from_clean = exempt_list
 
       performer.perform false do
         remove_extra_files
@@ -138,4 +139,44 @@ describe BowerRails::Performer do
     end
   end
 
+  describe "resolve_asset_paths" do
+    let(:target_path) { "#{tmp_dir}/vendor/assets/bower_components/foobar/style.css" }
+
+    before do
+      FileUtils.mkdir_p(File.dirname(target_path))
+      FileUtils.cp("#{files_dir}/style.css", target_path)
+
+      performer.perform false do
+        resolve_asset_paths
+      end
+    end
+
+    it "removes .css file" do
+      expect(File).to_not exist(target_path)
+    end
+
+    it "creates .css.erb file" do
+      expect(File).to exist("#{target_path}.erb")
+    end
+
+    describe "created .erb file" do
+      subject { File.read "#{target_path}.erb" }
+
+      it "includes proper asset paths" do
+        expect(subject).to include "<%= asset_path 'foobar.png' %>"
+      end
+
+      it "does not include improper asset paths" do
+        expect(subject).not_to match(/<%= asset_path 'foobar.png[?#]/)
+      end
+
+      it "pushes url params after the erb block" do
+        expect(subject).to include "<%= asset_path 'foobar.png' %>?"
+      end
+
+      it "pushes url anchor after the erb block" do
+        expect(subject).to include "<%= asset_path 'foobar.png' %>#"
+      end
+    end
+  end
 end
